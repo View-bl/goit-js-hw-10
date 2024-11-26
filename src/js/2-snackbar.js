aeb64c9f5bf2d52fe33d8d002950b1182a0f9d34
Roman <romaitgo@gmail.com>
2024-11-26 02:54:59 +0100

    js10

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,41 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delayInput = event.target.elements.delay.value;
+  const state = event.target.elements.state.value;
+
+  const delay = Number(delayInput);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        icon: false,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: '❌',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        icon: false,
+      });
+    });
+});
